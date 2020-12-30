5496fd18edd0cf3ec6c802f5c54c67617db9863f
tktcorporation <tktcorporation.go@gmail.com>
2020-12-31 02:21:01 +0900

    some changes

--- config.fish
+++ config.fish
@@ -6,5 +6,12 @@ set PATH $HOME/.pub-cache/bin:$PATH
 set PATH $HOME/Library/Android/sdk/platform-tools:$PATH
 set PATH $HOME/.cargo/bin:$PATH
 
-eval (starship init fish | source)
-eval (direnv hook fish)
+# ----------
+# bobthefish config
+# ----------
+set -g fish_prompt_pwd_dir_length 0  # ディレクトリ省略しない
+set -g theme_newline_cursor yes  # プロンプトを改行した先に設ける
+set -g theme_display_git_master_branch yes  # git の branch 名を表示
+set -g theme_color_scheme dracula
+set -g theme_display_date no  # 時刻を表示しないように設定
+set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
